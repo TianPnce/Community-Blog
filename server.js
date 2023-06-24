bde503f64a6d1c64c9f7a05e5175aac45b6152ea
TianPnce <122128252+TianPnce@users.noreply.github.com>
2023-06-24 01:21:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5678;
+const PORT = process.env.PORT || 3001;
 
 const { User, Blog, Comment } = require("./models");
 
